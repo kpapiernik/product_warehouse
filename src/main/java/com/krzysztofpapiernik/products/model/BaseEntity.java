b463bf3d0ab17c549879681f9d29541d3af81020
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-08-29 23:50:41 +0200

    Added BaseEntity class to extract Entity id value

---
+++ src/main/java/com/krzysztofpapiernik/products/model/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.krzysztofpapiernik.products.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+}
