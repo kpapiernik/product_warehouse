0c55f345c4e0747133194751da74dfa9a873d781
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-08-29 23:52:43 +0200

    Changed Product class to adjust for BaseEntity class and added producer and stock relations

--- src/main/java/com/krzysztofpapiernik/products/model/Product.java
+++ src/main/java/com/krzysztofpapiernik/products/model/Product.java
@@ -2,23 +2,30 @@ package com.krzysztofpapiernik.products.model;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 
 @Entity
 @Table(name = "products")
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
+public class Product extends BaseEntity {
     private String name;
     private BigDecimal price;
 
+    //TODO Spróbować przerobić na relację many to many
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "category_id")
     Category category;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "producer_id")
+    Producer producer;
+
+    @OneToOne(mappedBy = "product")
+    private Stock stock;
 }
