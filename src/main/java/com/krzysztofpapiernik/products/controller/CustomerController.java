535c737d414bc00b5d17a0be44e6a2d39a50c0ff
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-08-30 23:45:56 +0200

    Added Customer Controller

---
+++ src/main/java/com/krzysztofpapiernik/products/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.krzysztofpapiernik.products.controller;
+
+import com.krzysztofpapiernik.products.controller.dto.ResponseDataDto;
+import com.krzysztofpapiernik.products.dto.CreateCustomerDto;
+import com.krzysztofpapiernik.products.dto.GetCustomerDto;
+import com.krzysztofpapiernik.products.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseDataDto<GetCustomerDto> addCustomer(@RequestBody CreateCustomerDto createCustomerDto){
+        return new ResponseDataDto<>(customerService.addCustomer(createCustomerDto));
+    }
+
+    @GetMapping
+    public ResponseDataDto<List<GetCustomerDto>> getAllCustomers(){
+        return new ResponseDataDto<>(customerService.getAll());
+    }
+}
