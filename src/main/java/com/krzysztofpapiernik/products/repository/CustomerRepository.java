e42cfadf33832285660a8190ff390c4e2f8fbd2b
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-08-30 23:30:18 +0200

    Added Customer repository

---
+++ src/main/java/com/krzysztofpapiernik/products/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.krzysztofpapiernik.products.repository;
+
+import com.krzysztofpapiernik.products.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerByEmail(String email);
+}
