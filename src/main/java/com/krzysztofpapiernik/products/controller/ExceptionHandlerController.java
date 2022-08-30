268121ddd60242518a5a5203224b37005426ce8e
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-08-30 23:46:28 +0200

    Added Exceptions Handler Controller

---
+++ src/main/java/com/krzysztofpapiernik/products/controller/ExceptionHandlerController.java
@@ -0,0 +1,18 @@
+package com.krzysztofpapiernik.products.controller;
+
+import com.krzysztofpapiernik.products.controller.dto.ResponseDataDto;
+import com.krzysztofpapiernik.products.validation.exception.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDataDto<?> validationExceptionHandler(ValidationException err){
+        return new ResponseDataDto<>(err.getMessage());
+    }
+}
