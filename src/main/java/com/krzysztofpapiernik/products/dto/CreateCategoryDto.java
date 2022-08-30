66852907a1ef29efce597bd11da744354b0f8dba
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-08-30 23:47:58 +0200

    Added Dto for creating category

---
+++ src/main/java/com/krzysztofpapiernik/products/dto/CreateCategoryDto.java
@@ -0,0 +1,33 @@
+package com.krzysztofpapiernik.products.dto;
+
+import com.krzysztofpapiernik.products.model.Category;
+import com.krzysztofpapiernik.products.validation.Validator;
+import com.krzysztofpapiernik.products.validation.exception.ValidationException;
+
+import java.util.HashMap;
+
+public record CreateCategoryDto(String name) {
+
+    public void validate(){
+        var errors = new HashMap<String, String>();
+        
+        if(name == null){
+            errors.put("name", "is null");
+        } else if (name.isEmpty()) {
+            errors.put("name", "is empty String");
+        } else if (name.length() > 50){
+            errors.put("name", "is too long, max available length is 50 characters");
+        }
+
+        if(!errors.isEmpty()){
+            throw new ValidationException(Validator.validationErrorsToMessage(errors));
+        }
+    }
+
+    public Category toCategory(){
+        return Category
+                .builder()
+                .name(name)
+                .build();
+    }
+}
