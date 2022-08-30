1b7e7054574c7d0a458c0e5a53bf1096e8d7c364
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-08-30 23:29:13 +0200

    Added relation with Customer Orders and method to convert Entity to Dto in Customer Entity class

--- src/main/java/com/krzysztofpapiernik/products/model/Customer.java
+++ src/main/java/com/krzysztofpapiernik/products/model/Customer.java
@@ -1,13 +1,18 @@
 package com.krzysztofpapiernik.products.model;
 
+import com.krzysztofpapiernik.products.dto.GetCustomerDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuperBuilder
 @NoArgsConstructor
@@ -22,4 +27,12 @@ public class Customer extends BaseEntity{
     @Column(unique = true)
     private String email;
     private LocalDate dateOfBirth;
+
+    @OneToMany(mappedBy = "customer")
+    @Builder.Default
+    private List<CustomerOrder> customerOrders = new ArrayList<>();
+
+    public GetCustomerDto toGetCustomerDto(){
+        return new GetCustomerDto(id, firstName, lastName, email);
+    }
 }
