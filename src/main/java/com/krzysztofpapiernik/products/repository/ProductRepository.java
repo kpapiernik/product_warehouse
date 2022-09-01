e631d02d252db4f8a5fda599afccfe43d33b6146
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-09-01 02:20:56 +0200

    Added findByNameAndCategoryIdAndProducerId method to Product Repository

--- src/main/java/com/krzysztofpapiernik/products/repository/ProductRepository.java
+++ src/main/java/com/krzysztofpapiernik/products/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.krzysztofpapiernik.products.repository;
 import com.krzysztofpapiernik.products.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByNameAndCategory_IdAndProducer_Id(String name, Long categoryId, Long producerId);
 }
