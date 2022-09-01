8e473fafdad5b1fa4d7c951087e8254cc54f175c
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-09-01 02:13:27 +0200

    Added method to find Category by name

--- src/main/java/com/krzysztofpapiernik/products/repository/CategoryRepository.java
+++ src/main/java/com/krzysztofpapiernik/products/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.krzysztofpapiernik.products.repository;
 import com.krzysztofpapiernik.products.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findCategoryByName(String name);
 }
