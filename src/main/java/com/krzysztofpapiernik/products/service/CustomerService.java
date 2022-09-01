6492f7b24f52cbbaf3ce922e36a525ad702c77a0
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-09-01 02:22:38 +0200

    Refactor addCustomer method and Added getCustomerById method

--- src/main/java/com/krzysztofpapiernik/products/service/CustomerService.java
+++ src/main/java/com/krzysztofpapiernik/products/service/CustomerService.java
@@ -4,12 +4,13 @@ import com.krzysztofpapiernik.products.dto.CreateCustomerDto;
 import com.krzysztofpapiernik.products.dto.GetCustomerDto;
 import com.krzysztofpapiernik.products.model.Customer;
 import com.krzysztofpapiernik.products.repository.CustomerRepository;
-import com.krzysztofpapiernik.products.service.exception.CustomerServiceException;
+import com.krzysztofpapiernik.products.exception.CustomerServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -19,14 +20,14 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
 
     public GetCustomerDto addCustomer(CreateCustomerDto createCustomerDto){
-        createCustomerDto.validate();
 
         if(customerRepository.findCustomerByEmail(createCustomerDto.email()).isPresent()){
             throw new CustomerServiceException("Provided email is taken");
         }
 
-        var customer = createCustomerDto.toCustomer();
-        return customerRepository.save(customer).toGetCustomerDto();
+        return customerRepository
+                .save(createCustomerDto.toCustomer())
+                .toGetCustomerDto();
     }
 
     public List<GetCustomerDto> getAll(){
@@ -35,6 +36,16 @@ public class CustomerService {
                 .stream()
                 .map(Customer::toGetCustomerDto)
                 .toList();
+    }
+
+    public GetCustomerDto getCustomerById(Long id){
+        if(customerRepository.findById(id).isEmpty()){
+            throw new CustomerServiceException(Map.of("id", "User with id: %s does not exist".formatted(id)));
+        }
+        return customerRepository
+                .findById(id)
+                .map(Customer::toGetCustomerDto)
+                .orElseThrow(() -> new CustomerServiceException(Map.of("id", "Cannot get user with id: %s".formatted(id))));
     }
 
 }
