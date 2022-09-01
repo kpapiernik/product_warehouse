838cfacfea5d1f252244c7ca7fd3e86c0abd0dd4
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-09-01 02:20:27 +0200

    Added findByName method to Producer Repository

--- src/main/java/com/krzysztofpapiernik/products/repository/ProducerRepository.java
+++ src/main/java/com/krzysztofpapiernik/products/repository/ProducerRepository.java
@@ -3,5 +3,9 @@ package com.krzysztofpapiernik.products.repository;
 import com.krzysztofpapiernik.products.model.Producer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProducerRepository extends JpaRepository<Producer, Long> {
+    Optional<Producer> findByName(String name);
+
 }
