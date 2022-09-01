dc6620d40fd71a19fe8dc8f8c83a20443deee547
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-09-01 02:18:49 +0200

    Added missing parts to UpdateStockPositionDto

--- src/main/java/com/krzysztofpapiernik/products/dto/UpdateStockPositionDto.java
+++ src/main/java/com/krzysztofpapiernik/products/dto/UpdateStockPositionDto.java
@@ -1,4 +1,22 @@
 package com.krzysztofpapiernik.products.dto;
 
+import com.krzysztofpapiernik.products.exception.ValidationException;
+import com.krzysztofpapiernik.products.model.Stock;
+
+import java.util.Map;
+
 public record UpdateStockPositionDto(Long productId, Integer quantity) {
+
+    public UpdateStockPositionDto {
+        if(quantity <=0 ){
+            throw new ValidationException(Map.of("quantity", "must be positive number"));
+        }
+    }
+
+    public Stock toStock(){
+        return Stock
+                .builder()
+                .quantity(quantity)
+                .build();
+    }
 }
