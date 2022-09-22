d8fa554c1f5f98fb91291e392cf1592487cca6e3
Krzysztof Papiernik <krzysztof.papiernik@me.com>
2022-09-22 21:07:36 +0200

    Expanded DTO validation to new cases

--- src/main/java/com/krzysztofpapiernik/products/dto/UpdateStockPositionDto.java
+++ src/main/java/com/krzysztofpapiernik/products/dto/UpdateStockPositionDto.java
@@ -3,13 +3,25 @@ package com.krzysztofpapiernik.products.dto;
 import com.krzysztofpapiernik.products.exception.ValidationException;
 import com.krzysztofpapiernik.products.model.Stock;
 
-import java.util.Map;
+import java.util.HashMap;
 
 public record UpdateStockPositionDto(Long productId, Integer quantity) {
 
     public UpdateStockPositionDto {
-        if(quantity <=0 ){
-            throw new ValidationException(Map.of("quantity", "must be positive number"));
+        var errors = new HashMap<String, String>();
+
+        if (productId == null) {
+            errors.put("productId", "is null");
+        }
+
+        if(quantity == null){
+            errors.put("quantity", "is null");
+        } else if (quantity <= 0) {
+            errors.put("quantity", "must be positive value");
+        }
+
+        if(!errors.isEmpty()){
+            throw new ValidationException(errors);
         }
     }
 
